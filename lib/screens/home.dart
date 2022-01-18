f881a0219da5ec02b30b9c6bc67cf2fbd385a79c
MataRed-dev <matared-dev@github.com>
2022-01-18 09:11:38 +0100

    Add file

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -44,21 +44,23 @@ class HomeScreen extends StatelessWidget {
           ),
         alignment: Alignment.center,
         child: ListView(
-                children: const [
-                  // Exemple de card widget
-                  // CardWidget(idService: ,libelle: '',description: '',date: '',type: ,nom: '',prenom: '',numDep: ,),
-                  CardWidget(idService: 1,libelle: 'Speedrun',description: 'description',date: '14/01/2022',type: 1,nom: 'Ma',prenom: 'Th',numDep: 34,),
-                  CardWidget(idService: 1,libelle: 'Speedrun',description: 'description',date: '14/01/2022',type: 0,nom: 'Ma',prenom: 'Th',numDep: 34,),
-                  CardWidget(idService: 1,libelle: 'Speedrun',description: 'description',date: '14/01/2022',type: 0,nom: 'Ma',prenom: 'Th',numDep: 34,),
-                  CardWidget(idService: 1,libelle: 'Speedrun',description: 'description',date: '14/01/2022',type: 1,nom: 'Ma',prenom: 'Th',numDep: 34,),
-                  CardWidget(idService: 1,libelle: 'Speedrun',description: 'description',date: '14/01/2022',type: 1,nom: 'Ma',prenom: 'Th',numDep: 34,),
-                  CardWidget(idService: 1,libelle: 'Speedrun',description: 'description',date: '14/01/2022',type: 1,nom: 'Ma',prenom: 'Th',numDep: 34,),
-                  CardWidget(idService: 1,libelle: 'Speedrun',description: 'description',date: '14/01/2022',type: 0,nom: 'Ma',prenom: 'Th',numDep: 34,),
-                  CardWidget(idService: 1,libelle: 'Speedrun',description: 'description',date: '14/01/2022',type: 0,nom: 'Ma',prenom: 'Th',numDep: 34,),
-                  CardWidget(idService: 1,libelle: 'Speedrun',description: 'description',date: '14/01/2022',type: 1,nom: 'Ma',prenom: 'Th',numDep: 34,),
-                  CardWidget(idService: 1,libelle: 'Speedrun',description: 'description',date: '14/01/2022',type: 1,nom: 'Ma',prenom: 'Th',numDep: 34,),
-                ],
-              ),
+
+          children: const [
+            // Exemple de card widget
+            // CardWidget(idService: ,libelle: '',description: '',date: '',type: ,nom: '',prenom: '',numDep: ,),
+            CardWidget(idService: 1,libelle: 'Ménage',description: 'Je sais pas faire mon ménage',date: '14/01/2022',type: 0,nom: 'Achour',prenom: 'Mouloud',numDep: 22,),
+            CardWidget(idService: 1,libelle: 'Course',description: 'Disponible pour amener des personnes faire leur courses',date: '14/01/2022',type: 1,nom: 'Crespo-mara',prenom: 'Audrey',numDep: 29,),
+            CardWidget(idService: 1,libelle: 'Covoiturage',description: 'Je dois aller de rennes a redon.',date: '14/01/2022',type: 0,nom: 'Boulot',prenom: 'Gilles',numDep: 35,),
+            CardWidget(idService: 1,libelle: 'Découverte patrimoine',description: 'Je suis disponible pour vous faire découvrir le patrimoine de la loire atlantique',date: '14/01/2022',type: 1,nom: 'Bern',prenom: 'Stephane',numDep: 44,),
+            CardWidget(idService: 1,libelle: 'Bingo',description: 'Recherche d\'une soirée bingo ',date: '14/01/2022',type: 0,nom: 'Foucault',prenom: 'Jean-pierre',numDep: 49,),
+            CardWidget(idService: 1,libelle: 'Speedrun Binding of isaac',description: 'Demandez-moi si vous vous voulez des conseils sur des methode pour speedrun le jeu',date: '14/01/2022',type: 1,nom: 'Roselmack',prenom: 'Harry',numDep: 50,),
+            CardWidget(idService: 1,libelle: 'Speedrun',description: 'description',date: '14/01/2022',type: 0,nom: 'Sublet',prenom: 'Alessandra',numDep: 56,),
+            CardWidget(idService: 1,libelle: 'Appartement ou maison à louer',description: 'Je met à disposition mes biens pour que vous puissez passez un séjour dans ce qu\'est le merveilleux département de le Rhone ',date: '14/01/2022',type: 1,nom: 'Plaza',prenom: 'Stephane',numDep: 69,),
+            CardWidget(idService: 1,libelle: 'Victime d\'arnaque ',description: 'Je viens d\'être victime d\'une arnaque que suis-je censé faire',date: '14/01/2022',type: 0,nom: 'Courbet',prenom: 'Julien',numDep: 75,),
+            CardWidget(idService: 1,libelle: 'Rénovation maison',description: 'J\'ai besoin de bras pour rénover une maison d\'une famille avec 3 enfants dans l\'Esonne',date: '14/01/2022',type: 1,nom: 'Damidot',prenom: 'Valérie',numDep: 91,),
+          ],
+
+        ),
       ),
     );
   }
---
+++ lib/widgets/card.dart
@@ -0,0 +1,116 @@
+import 'dart:core';
+
+import 'package:flutter/material.dart';
+
+class CardWidget extends StatelessWidget {
+  const CardWidget({
+    Key? key,
+    this.idService,
+    this.libelle,
+    this.description,
+    this.date,
+    this.type,
+    this.nom,
+    this.prenom,
+    this.numDep,
+  }) : super(key: key);
+
+  final int? idService;
+  final String? libelle;
+  final String? description;
+  final String? date;
+  final int? type;
+  final String? nom;
+  final String? prenom;
+  final int? numDep;
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 0.9,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(13.0),
+        ),
+        color: _getColor(),
+        elevation: 10,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+
+          children: <Widget>[
+            ListTile(
+              leading: const Icon(Icons.accessible_forward_sharp, size: 50),
+              title: Text(_getLibelle()+" "+_getType(), style: const TextStyle(color: Colors.white)),
+              subtitle:
+                Text(_getDescription()+
+                    "\n"+_getDate()+
+                    "\n"+_getPrenom()+" "+_getNom()+
+                    "\n"+_getDep().toString(),
+                    style: const TextStyle(color: Colors.white)
+                ),
+
+            ),/*ButtonTheme(
+              child: ButtonBar(
+                children: <Widget>[
+                  TextButton(
+                    child: const Text('Voir +', style: TextStyle(color: Colors.white)),
+                    onPressed: () {
+                      Fluttertoast.showToast(
+                          msg: "This is Toast message",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.CENTER,
+                          timeInSecForIosWeb: 1
+                      );},
+                  ),
+                ],
+              ),
+            ),*/
+          ],
+        ),
+      ),
+    );
+  }
+
+  factory CardWidget.fromJson(Map<String, dynamic> json) {
+    return CardWidget(
+      idService: json['idService'],
+      libelle: json['libelle'],
+      description: json['description'],
+      date : json['date'],
+      type : json['type'],
+      nom : json['nom'],
+      prenom : json['prenom'],
+      numDep : json['numDep'],
+    );
+  }
+  _getLibelle() => libelle;
+
+  _getDescription() => description;
+
+  _getDate() => date;
+
+  _getPrenom() => prenom;
+
+  _getNom() => nom;
+
+  _getDep() => numDep;
+
+  _getIdService() => idService;
+
+  _getColor(){
+    if ( type == 0 ) {
+      return Colors.darkblue;
+    } else {
+      return Colors.redDark;
+    }
+  }
+
+  _getType(){
+    if ( type == 0 ) {
+      return 'Demande';
+    } else {
+      return 'Proposition';
+    }
+  }
+
+}
