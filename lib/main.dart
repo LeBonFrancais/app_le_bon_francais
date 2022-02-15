d367ad8e26461e228be4a324e2d2d4d9f05b3200
MataRed-dev <matared-dev@github.com>
2022-02-15 18:44:33 +0100

    Débogage des routes

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,15 @@ class _MyAppState extends State<MyApp> {
         '/SplashScreen': (context) => const SplashScreen(),
         '/': (context) => const HomeScreen(),
         'MapScreen': (context) => const MapScreen(),
+
+        //Si connecté afficher la page
         'NewService': (context) => const NewService(),
+        //Sinon rediriger vers page de connexion
+        //redirect -> LoginScreen
+
         'MailScreen': (context) => const MailScreen(),
         'WebViewApp': (context) => const WebViewApp(),
+
         //Condition si connecté
         'ProfilePage': (context) => const ProfilePage(),
         //Condition si non connecté
